6288a4147de4b80f02fcc506509625bebb109f19
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2016-01-04 15:04:23 +0300

    Initial Commit

---
+++ src/Hosts/ReactiveWebServer.ConsoleHost/AppInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace AnywayAnyday.ReactiveWebServer.ConsoleHost
+{
+    public sealed class AppInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            
+        }
+    }
+}
